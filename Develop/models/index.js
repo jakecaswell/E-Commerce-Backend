900760f37e3ae84508ce9e8ac22e6b5f1e2716d1
jcaswell <jakecaswell88@gmail.com>
2021-11-22 21:53:55 -0500

    fixed some routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -15,15 +15,16 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_id',
+  // as: 'product_id',
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  // as: 'tag',
   foreignKey: 'tag_id'
 });
+
 
 module.exports = {
   Product,
